4aecca16b7a4b5cd2bc4f7be44a9dbf1cfc34af5
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-09 19:25:02 +0700

    first commit

---
+++ TBO_TUGAS 5.A PYTHON KALVIN.py
@@ -0,0 +1,16 @@
+#.TUGAS 5.A - Kuliah TBO
+#.DOSEN = Arnes Y.V, M.Kom
+#. FOR...ELSE
+print("NAMA = NIKOLAS KALVIN");
+print("NPM = 21421002");
+#
+#input data
+listKota = [
+    'Jakarta', 'Surabaya', 'Depok', 'Bekasi', 'Solo',
+    'Jogjakarta', 'Semarang', 'Makassar'
+]
+
+for kota in listKota:
+    print(kota)
+else:
+    print('Tidak ada lagi item yang tersisa, ulang lagi ya')    
